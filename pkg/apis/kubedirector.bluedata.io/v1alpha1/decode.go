5bcee5008d364c5530cdb4abf3c543ec164cdb0d
Krishna Sagiraju <krishna@bluedata.com>
2018-12-20 13:13:09 -0500

    Updated Image and SetupPackage types.

--- pkg/apis/kubedirector.bluedata.io/v1alpha1/decode.go
+++ pkg/apis/kubedirector.bluedata.io/v1alpha1/decode.go
@@ -16,29 +16,64 @@ package v1alpha1
 
 import "encoding/json"
 
-// UnmarshalJSON for JSONSetupPackage handles the unmarshalling of three
+// UnmarshalJSON for SetupPackage handles the unmarshalling of three
 // scenarios wrt 'setup_package':
 //   1. omitted                 : IsSet==false
 //   2. explicitly set to null  : IsSet==true && IsNull==true
 //   3. Set to a valid object   : IsSet=true && IsNull==false
-func (jsonSetupPackage *JSONSetupPackage) UnmarshalJSON(
+func (setupPackage *SetupPackage) UnmarshalJSON(
 	data []byte,
 ) error {
 
-	// The fact that we entered this function means the filed is set oherwise,
+	// The fact that we entered this function means the filed is set otherwise,
 	// this field will be false by default.
-	jsonSetupPackage.IsSet = true
+	setupPackage.IsSet = true
 
-	if string(data) == "null" {
+	if (string(data) == "null") || (string(data) == "") {
 		// The field value is explicitly set to null
-		jsonSetupPackage.IsNull = true
+		setupPackage.IsNull = true
 		return nil
 	}
 
-	if err := json.Unmarshal(data, &jsonSetupPackage.SetupPackage); err != nil {
+	if err := json.Unmarshal(data, &setupPackage.PackageURL); err != nil {
 		return err
 	}
-	jsonSetupPackage.IsNull = false
+	setupPackage.IsNull = false
 
 	return nil
 }
+
+// UnmarshalJSON for Image handles the unmarshalling of three
+// scenarios wrt 'image_repo_tag':
+//   1. omitted                 : IsSet==false
+//   2. explicitly set to null  : IsSet==true && IsNull==true
+//   3. Set to a valid object   : IsSet=true && IsNull==false
+func (image *Image) UnmarshalJSON(
+	data []byte,
+) error {
+
+	// The fact that we entered this function means the filed is set otherwise,
+	// this field will be false by default.
+	image.IsSet = true
+
+	if (string(data) == "null") || (string(data) == "") {
+		// The field value is explicitly set to null
+		image.IsNull = true
+		return nil
+	}
+
+	if err := json.Unmarshal(data, &image.RepoTag); err != nil {
+		return err
+	}
+	image.IsNull = false
+
+	return nil
+}
+
+// // MarshalJSON is to handle encoding the Image into json.
+// func (image Image) MarshalJSON() ([]byte, error) {
+// 	if image.IsSet == false {
+// 		return json.Marshall(nil)
+// 	}
+// 	return
+// }
--- pkg/apis/kubedirector.bluedata.io/v1alpha1/types.go
+++ pkg/apis/kubedirector.bluedata.io/v1alpha1/types.go
@@ -126,18 +126,18 @@ type KubeDirectorApp struct {
 
 // AppSpec is the spec provided for an app definition.
 type AppSpec struct {
-	Label            Label            `json:"label"`
-	DistroID         string           `json:"distro_id"`
-	Version          string           `json:"version"`
-	SchemaVersion    int              `json:"schema_version"`
-	Image            Image            `json:"image,omitempty"`
-	JSONSetupPackage JSONSetupPackage `json:"setup_package,omitempty"`
-	Services         []Service        `json:"services"`
-	NodeRoles        []NodeRole       `json:"roles"`
-	Config           NodeGroupConfig  `json:"config"`
-	PersistDirs      *[]string        `json:"persist_dirs"`
-	Capabilities     []v1.Capability  `json:"capabilities"`
-	SystemdRequired  bool             `json:"systemdRequired"`
+	Label           Label           `json:"label"`
+	DistroID        string          `json:"distro_id"`
+	Version         string          `json:"version"`
+	SchemaVersion   int             `json:"schema_version"`
+	Image           Image           `json:"image_repo_tag,omitempty"`
+	SetupPackage    SetupPackage    `json:"config_package_url",omitempty"`
+	Services        []Service       `json:"services"`
+	NodeRoles       []NodeRole      `json:"roles"`
+	Config          NodeGroupConfig `json:"config"`
+	PersistDirs     *[]string       `json:"persist_dirs"`
+	Capabilities    []v1.Capability `json:"capabilities"`
+	SystemdRequired bool            `json:"systemdRequired"`
 }
 
 // Label is a short name and long description for the app definition.
@@ -150,21 +150,18 @@ type Label struct {
 // specified, and/or a role-specific image that will override any top-level
 // image.
 type Image struct {
-	RepoTag string `json:"repoTag"`
+	IsSet   bool
+	IsNull  bool
+	RepoTag string
 }
 
 // SetupPackage describes the app setup package to be used. A top-level
 // package can be specified, and/or a role-specific package that will override
 // any top-level package.
 type SetupPackage struct {
-	ImportUrl string `json:"import_url"`
-}
-
-// JSONSetupPackage is a wrapper around SetupPackage
-type JSONSetupPackage struct {
-	IsSet        bool
-	IsNull       bool
-	SetupPackage SetupPackage
+	IsSet      bool
+	IsNull     bool
+	PackageURL string
 }
 
 // Service describes a network endpoint that should be exposed for external
@@ -189,11 +186,11 @@ type ServiceEndpoint struct {
 // the same services. At deployment time all role members will receive
 // identical resource assignments.
 type NodeRole struct {
-	ID               string           `json:"id"`
-	Cardinality      string           `json:"cardinality"`
-	Image            Image            `json:"image,omitempty"`
-	JSONSetupPackage JSONSetupPackage `json:"setup_package,omitempty"`
-	PersistDirs      *[]string        `json:"persist_dirs"`
+	ID           string       `json:"id"`
+	Cardinality  string       `json:"cardinality"`
+	Image        Image        `json:"image_repo_tag,omitempty"`
+	SetupPackage SetupPackage `json:"config_package_url,omitempty"`
+	PersistDirs  *[]string    `json:"persist_dirs"`
 }
 
 // NodeGroupConfig identifies a set of roles, and the services on those roles.

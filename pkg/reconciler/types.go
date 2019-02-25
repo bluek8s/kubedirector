a7ff1c071f1a4b26970f41f2e702ba1d01a90591
Krishna Sagiraju <krishna@bluedata.com>
2019-02-25 13:13:13 -0500

    Configcli tarball creates a versioned dir when extracted.

--- pkg/reconciler/types.go
+++ pkg/reconciler/types.go
@@ -73,8 +73,8 @@ const (
 	nodePrepSrcFile    = "/root/configcli.tgz"
 	nodePrepDestFile   = "/tmp/configcli.tgz"
 	nodePrepInstallCmd = `cd /tmp;tar xzf configcli.tgz;
-	chmod +x /tmp/configcli/install;/tmp/configcli/install;
-	rm -rf /tmp/configcli;rm -f /tmp/configcli.tgz;
+	chmod +x /tmp/configcli-*/install;/tmp/configcli-*/install;
+	rm -rf /tmp/configcli-*;rm -f /tmp/configcli.tgz;
 	ln -sf /usr/bin/configcli /usr/bin/bdvcli;
 	ln -sf /usr/bin/configcli /usr/bin/bd_vcli;`
 	nodePrepTestFile   = "/usr/bin/configcli"

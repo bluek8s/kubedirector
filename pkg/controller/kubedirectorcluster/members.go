7028160fd3cf5d7ff4542d57e699c8188183d78f
Joel Baxter <joel.baxter@hpe.com>
2020-01-24 10:15:37 -0800

    dial back error logging in this case
    Not necessarily an error.

--- pkg/controller/kubedirectorcluster/members.go
+++ pkg/controller/kubedirectorcluster/members.go
@@ -188,16 +188,28 @@ func handleCreatePendingMembers(
 			defer wgRunning.Done()
 			pod, podGetErr := observer.GetPod(cr.Namespace, m.Pod)
 			if podGetErr != nil {
-				// Can't get the pod. Skip it and try again later.
-				shared.LogErrorf(
-					reqLogger,
-					podGetErr,
-					cr,
-					shared.EventReasonMember,
-					"failed to find member{%s} in role{%s}",
-					m.Pod,
-					role.roleStatus.Name,
-				)
+				// Can't get the pod. Skip it and try again later. This is
+				// not necessarily an error; K8s might be slow.
+				if errors.IsNotFound(podGetErr) {
+					shared.LogInfof(
+						reqLogger,
+						cr,
+						shared.EventReasonMember,
+						"failed to find member{%s} in role{%s}; will retry",
+						m.Pod,
+						role.roleStatus.Name,
+					)
+				} else {
+					shared.LogErrorf(
+						reqLogger,
+						podGetErr,
+						cr,
+						shared.EventReasonMember,
+						"failed to find member{%s} in role{%s}",
+						m.Pod,
+						role.roleStatus.Name,
+					)
+				}
 				return
 			}
 			if pod.Status.Phase == corev1.PodRunning {

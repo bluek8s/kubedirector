4502166dedc734eb866f5bafbc98ecc0d21645d3
Joel Baxter <joel.baxter@neogeographica.com>
2020-02-09 10:24:45 -0800

    more human-readable state strings

--- pkg/controller/kubedirectorcluster/types.go
+++ pkg/controller/kubedirectorcluster/types.go
@@ -43,13 +43,13 @@ const (
 type memberState string
 
 const (
-	memberCreatePending memberState = "create_pending"
+	memberCreatePending memberState = "create pending"
 	memberCreating                  = "creating"
-	memberConfigured                = "configured_internal" // not externally visible
+	memberConfigured                = "configured" // not externally visible
 	memberReady                     = "ready"
-	memberDeletePending             = "delete_pending"
+	memberDeletePending             = "delete pending"
 	memberDeleting                  = "deleting"
-	memberConfigError               = "config_error"
+	memberConfigError               = "config error"
 )
 
 const (

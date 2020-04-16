b33982423ccbb343f28ccfd7844a612c2f20927c
rkishore-hpe <ramaswami.kishore@hpe.com>
2020-04-16 16:08:34 -0700

    Undoing a test change

--- pkg/catalog/interrogate.go
+++ pkg/catalog/interrogate.go
@@ -113,7 +113,6 @@ func GetRoleCardinality(
 
 // GetRoleMinResources is a utility function that fetching the minimum resources
 // for a given app role
-// Test comment XXX
 func GetRoleMinResources(
 	appRole *kdv1.NodeRole,
 ) *v1.ResourceList {

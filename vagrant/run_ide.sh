47a4f3da6711aa5ff4eefe40dedc134255c54c78
Chris Snow <chsnow123@gmail.com>
2020-08-25 20:50:49 +0100

    fix operator-sdk generate k8s

--- vagrant/run_ide.sh
+++ vagrant/run_ide.sh
@@ -6,14 +6,11 @@ then
     exit 1
 fi
 
-# configure Theia
-SHELL=/bin/bash \
-THEIA_DEFAULT_PLUGINS=local-dir:/vagrant/vagrant/plugins  \
+SHELL=/bin/bash
+THEIA_DEFAULT_PLUGINS=local-dir:/home/vagrant/plugins 
 GOPATH=/home/project
 PATH=$PATH:$GOPATH/bin
 
 cd /home/vagrant
-yarn
-yarn theia build
 
 node ./src-gen/backend/main.js /vagrant --hostname=0.0.0.0

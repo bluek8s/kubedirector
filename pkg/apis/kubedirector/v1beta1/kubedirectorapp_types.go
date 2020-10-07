76de4a6ea1083f7c1ee8a18395a6b480a4bb99a5
koshiksamota10 <60718736+koshiksamota10@users.noreply.github.com>
2020-10-07 15:25:06 -0700

    Add a field inside the app crd for path where we can retrieve app specific logos.

--- pkg/apis/kubedirector/v1beta1/kubedirectorapp_types.go
+++ pkg/apis/kubedirector/v1beta1/kubedirectorapp_types.go
@@ -34,6 +34,7 @@ type KubeDirectorAppSpec struct {
 	DefaultEventList    *[]string           `json:"defaultEventList,omitempty"`
 	Capabilities        []corev1.Capability `json:"capabilities,omitempty"`
 	SystemdRequired     bool                `json:"systemdRequired,omitempty"`
+	DefaultLogoURL      string              `json:"defaultLogoURL,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
